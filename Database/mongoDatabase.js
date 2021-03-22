eb17cc7f9b027b13c2e13f9b370465951bcf1e5e
kavinda ravishan <kvnravishan@outlook.com>
2021-03-22 17:21:35 +0530

    mogo url and env

--- Database/mongoDatabase.js
+++ Database/mongoDatabase.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.DB_CONNECTION,
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e8ji8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
     console.log("Connected to Mondoose DB!");
