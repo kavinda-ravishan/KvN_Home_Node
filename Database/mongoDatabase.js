0f82346408e32976c551dbd380e2f47ecb2782b4
Kavinda Ravishan <68157639+kavinda-ravishan@users.noreply.github.com>
2022-05-29 20:01:33 +0530

    mongo db name chaned

--- Database/mongoDatabase.js
+++ Database/mongoDatabase.js
@@ -20,7 +20,7 @@ function mongoDBStatus(mongoDBStatusCode) {
 }
 
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e8ji8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e8ji8.mongodb.net/webChatApp?retryWrites=true&w=majority`,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
     console.log(
